97cdd248f2bacbaa6a10a882d1f5d140ebea5630
Thiago Andrade Buosi da Silva <thiagoabuosi19@gmail.com>
2022-04-13 15:59:56 -0300

    Resolução do Vamos Praticar sobre getElementById

---
+++ fundamentos/bloco-5-dom-eventos-web-storage/dia-1-dom-seletores/vamosPraticar.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "Harry Potter";
+
+document.getElementById("second-paragraph").innerText = "Vamos Brincar";
+
+document.getElementById("subtitle").innerText = "Teste 123";
