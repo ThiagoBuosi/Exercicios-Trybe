973f1eb79cc9f8b789bc77188f2dab4adcbdcb91
Thiago Andrade Buosi da Silva <thiagoabuosi19@gmail.com>
2022-04-13 19:41:27 -0300

    Resolução do vamos praticar parte II

--- fundamentos/bloco-5-dom-eventos-web-storage/dia-1-dom-seletores/parte2.js
+++ fundamentos/bloco-5-dom-eventos-web-storage/dia-1-dom-seletores/parte2.js
@@ -3,3 +3,7 @@ document.getElementById("page-title").innerText = "Harry Potter";
 document.getElementById("second-paragraph").innerText = "Vamos Brincar";
 
 document.getElementById("subtitle").innerText = "Teste 123";
+
+document.getElementsByClassName("paragraph")[0].style.color = "red";
+
+document.getElementsByTagName("h4")[0].style.color = "green";
